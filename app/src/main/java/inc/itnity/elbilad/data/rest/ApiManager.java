d5557ee113ff084fde99eb98f226283832384bed
st1ch <artemg@tallium.com>
2017-01-14 18:06:16 +0200

    news items

---
+++ app/src/main/java/inc/itnity/elbilad/data/rest/ApiManager.java
@@ -0,0 +1,8 @@
+package inc.itnity.elbilad.data.rest;
+
+/**
+ * Created by st1ch on 11.01.17.
+ */
+
+public class ApiManager {
+}
---
+++ app/src/main/java/inc/itnity/elbilad/data/rest/api/ElbiladAPI.java
@@ -0,0 +1,18 @@
+package inc.itnity.elbilad.data.rest.api;
+
+/**
+ * Created by st1ch on 11.01.17.
+ */
+
+public interface ElbiladAPI {
+
+  String ARTICLE = "/article";
+  String ARTICLE_ID = "/article_id";
+
+  String CATEGORIE = "/categorie";
+  String CATEGORIE_ID = "/categorie_id";
+
+  String PATH_ARTICLE_ID = "/{" + ARTICLE_ID + "}";
+
+
+}
