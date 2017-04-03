63eb8fdead2f6b688a13be53af4b550faa6bf09d
st1ch <artemg@tallium.com>
2017-04-03 11:23:59 +0300

    main menu scroll hotfix

--- app/src/main/java/inc/itnity/elbilad/presentation/activities/MainActivity.java
+++ app/src/main/java/inc/itnity/elbilad/presentation/activities/MainActivity.java
@@ -4,8 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.NestedScrollView;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -17,17 +17,28 @@ public class MainActivity extends AbstractBaseActivity {
   private final String TAG = this.getClass().getSimpleName();
 
   @Nullable @BindView(R.id.drawer_layout) DrawerLayout mDrawer;
+  @Nullable @BindView(R.id.nested_scroll_view) NestedScrollView mainMenuScrollView;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
     String pushArticle = preferenceHelper.getArticleId();
-    if(!TextUtils.isEmpty(pushArticle)){
+    if (!TextUtils.isEmpty(pushArticle)) {
       fragmentNavigator.startArticleDetailsFragment(pushArticle);
     } else {
       fragmentNavigator.startHomeScreenFragment();
     }
 
+    if (mDrawer != null) {
+      mDrawer.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+        @Override public void onDrawerOpened(View drawerView) {
+          super.onDrawerOpened(drawerView);
+          if (mainMenuScrollView != null) {
+            mainMenuScrollView.scrollTo(0, 0);
+          }
+        }
+      });
+    }
   }
 
   @Override protected void onDestroy() {
